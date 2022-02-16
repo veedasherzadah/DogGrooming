e68e16f53ca4850e0f7d4509c154c6bd09acd745
veedasherzadah <veedasherzadah1416@gmail.com>
2022-02-16 00:52:15 -0500

    things look nicer

--- src/main/webapp/app/modal/modal-details-folder/modal-details-form/modal-details-form.component.ts
+++ src/main/webapp/app/modal/modal-details-folder/modal-details-form/modal-details-form.component.ts
@@ -10,18 +10,16 @@ import {NgbDateStruct, NgbCalendar, NgbDate} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./modal-details-form.component.scss']
 })
 export class ModalDetailsFormComponent{
-  // @Input() continue; 
   userDetailsFormGroup!: FormGroup;
   servicesFormGroup!: FormGroup;
   calendarFormGroup!: FormGroup;
-  model!: NgbDateStruct;
+
   date!: {year: number, month: number};
   minDate!: NgbDate;
-  TIME_BUTTON = 'time-button';
-  TIME_BUTTON_CLICKED = 'time-button-clicked';
-  showErrors = false;
+ 
   pages = ['userDetails', 'services', 'calendar']
-  page = this.pages[2];
+  page = this.pages[0];
+
   @Output() getButtonDisabled = new EventEmitter<boolean>();
 
   Services: Array<any> = [
@@ -43,17 +41,40 @@ export class ModalDetailsFormComponent{
   constructor(private fb: FormBuilder, private calendar: NgbCalendar) {
    this.createForm();
    this.minDate = calendar.getToday();
+
+   this.userDetailsFormGroup.statusChanges.subscribe(res => {
+    // console.warn(this.userDetailsFormGroup)
+    this.getButtonDisabled.emit(res === 'INVALID')
+  })
+
+   this.servicesFormGroup.statusChanges.subscribe(res => {
+    console.warn(this.servicesFormGroup.get('service')?.value)
+    if(this.servicesFormGroup.get('service')?.value !== '') {
+      this.getButtonDisabled.emit(false);
+    }
+    // const checkArray: FormArray = this.servicesFormGroup.get('checkArray') as FormArray;
+    // if(checkArray.length === 0){
+    //   this.getButtonDisabled.emit(true);
+    // }
+    // else {
+    //   this.getButtonDisabled.emit(false);
+    // }
+  })
+    this.calendarFormGroup.statusChanges.subscribe(res => {
+      console.warn(this.calendarFormGroup.get('calendar')?.value);
+      console.warn(this.calendarFormGroup.get('time')?.value);
+
+      if(this.calendarFormGroup.get('calendar')?.value !== null && this.calendarFormGroup.get('time')?.value !== '') {
+        this.getButtonDisabled.emit(false);
+      }
+    })
  }
-
-//  ngOnInit(): void {
-
-//  }
 
   createForm():void {
    this.userDetailsFormGroup = this.fb.group({
     email: new FormControl('', [
       Validators.required,
-      Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/),
+      // Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/),
     ]),
     firstName: new FormControl('', [
       Validators.required
@@ -73,7 +94,10 @@ export class ModalDetailsFormComponent{
    });
 
    this.servicesFormGroup = this.fb.group({
-    checkArray: this.fb.array([])
+    // checkArray: this.fb.array([])
+    service: new FormControl('', [
+      Validators.required
+    ]),
    });
 
    this.calendarFormGroup = this.fb.group({
@@ -87,45 +111,48 @@ export class ModalDetailsFormComponent{
    
  }
 
- ngAfterViewInit(): void {
-   if(this.page === this.pages[0]){
-    this.userDetailsFormGroup.statusChanges.subscribe(res => {
-      this.getButtonDisabled.emit(res === 'INVALID')
-      // console.warn(res === 'INVALID')
-    })
-   }
+ 
+//  ngAfterViewInit(): void {
+//    console.warn("in here")
+//    if(this.page === this.pages[0]){
+//     this.userDetailsFormGroup.statusChanges.subscribe(res => {
+//       // console.warn(this.userDetailsFormGroup)
+//       this.getButtonDisabled.emit(res === 'INVALID')
+//     })
+//    }
 
-   if(this.page === this.pages[1]){
-    this.servicesFormGroup.statusChanges.subscribe(res => {
-      const checkArray: FormArray = this.servicesFormGroup.get('checkArray') as FormArray;
-      if(checkArray.length === 0){
-        this.getButtonDisabled.emit(true);
-      }
-      else {
-        this.getButtonDisabled.emit(false);
-      }
-    })
-   }
+//    if(this.page === this.pages[1]){
+//     this.servicesFormGroup.statusChanges.subscribe(res => {
+//       console.warn(this.servicesFormGroup.get('service')?.value)
+//       if(this.servicesFormGroup.get('service')?.value !== '') {
+//         this.getButtonDisabled.emit(false);
+//       }
+//       // const checkArray: FormArray = this.servicesFormGroup.get('checkArray') as FormArray;
+//       // if(checkArray.length === 0){
+//       //   this.getButtonDisabled.emit(true);
+//       // }
+//       // else {
+//       //   this.getButtonDisabled.emit(false);
+//       // }
+//     })
+//    }
 
-   if(this.page === this.pages[2]){
-     console.warn(this.calendarFormGroup);
-    this.calendarFormGroup.statusChanges.subscribe(res => {
-      console.warn(this.calendarFormGroup.value);
-      if(this.calendarFormGroup.get('calendar')?.value !== null && this.calendarFormGroup.get('time')?.value !== '') {
-        this.getButtonDisabled.emit(false);
-      }
-    })
-   }
+//    if(this.page === this.pages[2]){
+//     this.calendarFormGroup.statusChanges.subscribe(res => {
+//       console.warn(this.calendarFormGroup.get('calendar')?.value);
+//       console.warn(this.calendarFormGroup.get('time')?.value);
+
+//       if(this.calendarFormGroup.get('calendar')?.value !== null && this.calendarFormGroup.get('time')?.value !== '') {
+//         this.getButtonDisabled.emit(false);
+//       }
+//     })
+//    }
   
-}
+// }
   continueClicked():void {
     const index = this.pages.indexOf(this.page);
     this.page = this.pages[index + 1];
-    // this.continue = true;
-    // if(this.formGroup.invalid){
-    //   this.showErrors = true;
-    // }
-    // console.warn(this.formGroup);
+    this.getButtonDisabled.emit(true);
   }
 
   onCheckboxChange(e: any):void {
@@ -141,17 +168,6 @@ export class ModalDetailsFormComponent{
         }
         i++;
       });
-    }
-  }
-
-  setTime(time:string, id:string): void {
-    this.calendarFormGroup.get('time')?.setValue(time);
-    const element = window.document.getElementById(id);
-    if(element?.className === this.TIME_BUTTON) {
-      element.className = this.TIME_BUTTON_CLICKED;
-    }
-    else {
-      element!.className = this.TIME_BUTTON;
     }
   }
 }
